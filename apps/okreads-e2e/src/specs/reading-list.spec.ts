7284049089ff89f1bf2c43a62c79b64d24087281
Venkata Vikas Bodempudi <vikas.bodempudi@sprint.com>
2020-10-16 14:05:28 -0500

    undo action

--- apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -1,4 +1,4 @@
-import { $, browser, ExpectedConditions } from 'protractor';
+import { $, browser, ExpectedConditions, $$, by } from 'protractor';
 
 describe('When: I use the reading list feature', () => {
   it('Then: I should see my reading list', async () => {
@@ -18,3 +18,45 @@ describe('When: I use the reading list feature', () => {
     );
   });
 });
+
+
+describe('When: I use the want to read to see the snack bar', () => {
+  it('Then: I should see snackbar', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+
+    const form = await $('form');
+    await $('input[type="search"]').sendKeys('angularJS');
+    await form.submit();
+    const items = await $$('[data-testing="book-item"]');
+    expect(items.length).toBeGreaterThan(1);
+    await $('[data-testing="want-to-read-button"]').click();
+    await browser.wait(
+      ExpectedConditions.presenceOf($('.mat-simple-snackbar'))
+    );
+  });
+});
+
+describe('When: I use the remove feature list feature and undo the remove on snackbar', () => {
+  it('Then: I should see snackbar and undo', async () => {
+
+
+    await $('[data-testing="toggle-reading-list"]').click();
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        $('[data-testing="reading-list-container"]'),
+        'My Reading List'
+      )
+    );
+    const readinglistItem = await $$('[data-testing="reading-listitem"]');
+    expect(readinglistItem.length).toEqual(1);
+    await $('[data-testing="remove-book-from-list"]').click();
+    const undoAction = await browser.driver.findElement(by.css('.mat-simple-snackbar-action'));
+    await undoAction.click();
+  });
+});
+
+
+
--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { getReadingList, removeFromReadingList, undoActionRemoveReadingList } from '@tmo/books/data-access';
 import { ReadingListItem } from '@tmo/shared/models';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'tmo-reading-list',
   templateUrl: './reading-list.component.html',
@@ -9,10 +10,23 @@ import { ReadingListItem } from '@tmo/shared/models';
 })
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
+  constructor(private readonly store: Store, private snackBar: MatSnackBar) {}
 
-  constructor(private readonly store: Store) {}
-
+  addSnackbar(book) {
+    const snackBarRef = this.snackBar.open(
+      `${book.title} Book has been Removed from the Reading List!`,
+      'Undo',
+      {
+        duration: 3000
+      }
+    )
+    snackBarRef.onAction().subscribe(
+      () =>
+      this.store.dispatch(undoActionRemoveReadingList({ book }))
+    )
+}
   removeFromReadingList(item: ReadingListItem) {
     this.store.dispatch(removeFromReadingList({ item }));
+    this.addSnackbar(item);
   }
 }
