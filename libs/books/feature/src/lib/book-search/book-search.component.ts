1b6ad088d246969a021b82581aa654504170f9cf
Venkata Vikas Bodempudi <vikas.bodempudi@sprint.com>
2020-12-14 12:57:08 -0600

    addressing comments

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -9,7 +9,7 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
-import { Observable } from 'rxjs/internal/Observable';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
